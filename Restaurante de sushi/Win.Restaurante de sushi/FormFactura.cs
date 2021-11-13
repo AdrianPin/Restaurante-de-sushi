7cb992c646d105a9d5e0aa893194cdd1ac240d80
adria <adria@DESKTOP-AVIA2KN>
2021-11-13 00:57:45 -0500

    Proyecto restaurante de sushi

---
+++ Restaurante de sushi/Win.Restaurante de sushi/FormFactura.cs
@@ -0,0 +1,85 @@
+﻿using BL.Sushi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Restaurante_de_sushi
+{
+    public partial class FormFactura : Form
+    {
+        FacturaBL _facturaBL;
+        ClientesBL _clientesBL;
+        FoodMenuBL _foodmenuBL;
+        
+
+        public  FormFactura()
+        {
+            InitializeComponent();
+
+            _facturaBL = new FacturaBL();
+            listaFacturasBindingSource.DataSource = _facturaBL.ObtenerFacturas();
+
+            _clientesBL = new ClientesBL();
+            listaClientesBindingSource.DataSource = _clientesBL.ObtenerClientes();
+
+           
+        }
+
+        private void FormFactura_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _facturaBL.AgregarFactura();
+            listaFacturasBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+       private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listaFacturasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaFacturasBindingSource.EndEdit();
+
+            var factura = (Factura)listaFacturasBindingSource.Current;
+            var resultado = _facturaBL.GuardarFactura(factura);
+
+            if (resultado.Exitoso == true)
+            {
+                listaFacturasBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Factura Guardada");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+    }
+    }
+
